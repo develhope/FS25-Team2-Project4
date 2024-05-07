1c39929ea4d04f689a70318dc924d705bad7e48b
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-05-07 18:51:45 +0200

    set structure project

---
+++ recipe-roulette.js/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h2>My App</h2>
+  )
+}
+
+export default App
---
+++ recipe-roulette.js/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Root } from './Root.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+)
