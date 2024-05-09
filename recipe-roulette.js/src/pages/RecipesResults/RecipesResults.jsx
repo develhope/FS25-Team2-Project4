aef0e93185e2ccf1e4e6936ac0fdffbda18c78f1
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-05-09 17:41:25 +0200

    RecipeCards + styles, added emty pages and navigation buttons

--- recipe-roulette.js/src/pages/RecipesResults/RecipesResults.jsx
+++ recipe-roulette.js/src/pages/RecipesResults/RecipesResults.jsx
@@ -3,6 +3,7 @@ import RecipeCard from "../../components/authentication/RecipeCard/RecipeCard"
 function RecipeResults() {
     return (
         <>
+        <h1>Recipe Results</h1>
             <RecipeCard title="Just a title" />
             <RecipeCard title="Just another title" />
             <RecipeCard title="Just anotherother title" />
