2e5ca0cad6d5a26467c15dab4b615b27ea419fc6
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-05-16 10:18:14 +0200

    minor changes

--- recipe-roulette.js/src/components/IngredientCard/useIngredientCard.jsx
+++ recipe-roulette.js/src/components/IngredientCard/useIngredientCard.jsx
@@ -8,52 +8,52 @@ export function useIngredientCard(label, id, isSelected, bgColor) {
         randomIngredients,
         handleIngredientUpdate,
         handleIngredientsDecrement,
-    } = useManageIngredients();
+    } = useManageIngredients()
 
-    const [inputValues, setInputValues] = useState({ initial: label, current: label });
-    const [cardState, setCardState] = useState({ label, state: isSelected, id, color: bgColor, inputActive: false });
-    const [suggestions, setSuggestions] = useState(ingredients);
+    const [inputValues, setInputValues] = useState({ initial: label, current: label })
+    const [cardState, setCardState] = useState({ label, state: isSelected, id, color: bgColor, inputActive: false })
+    const [suggestions, setSuggestions] = useState(ingredients)
 
     useEffect(() => {
-        setInputValues(prev => ({ ...prev, current: label }));
-        setCardState(prev => ({ ...prev, label, state: isSelected, id, color: bgColor }));
+        setInputValues(prev => ({ ...prev, current: label }))
+        setCardState(prev => ({ ...prev, label, state: isSelected, id, color: bgColor }))
     }, [label, bgColor, isSelected, id, displayedIngredients, randomIngredients])
 
     useEffect(() => {
-        setSuggestions(ingredients);
-    }, [ingredients]);
+        setSuggestions(ingredients)
+    }, [ingredients])
 
     function handleIngredientClick() {
-        const newState = !cardState.state;
-        setCardState(prev => ({ ...prev, state: newState }));
-        handleIngredientUpdate(newState, cardState.id);
+        const newState = !cardState.state
+        setCardState(prev => ({ ...prev, state: newState }))
+        handleIngredientUpdate(newState, cardState.id)
     }
 
     function handleInputActivation(e) {
-        e.stopPropagation();
-        setCardState(prev => ({ ...prev, inputActive: true }));
+        e.stopPropagation()
+        setCardState(prev => ({ ...prev, inputActive: true }))
 
         if (inputValues.current !== "") {
-            setInputValues(prev => ({ ...prev, initial: cardState.label, current: "" }));
-            handleIngredientUpdate(false, cardState.id);
+            setInputValues(prev => ({ ...prev, initial: cardState.label, current: "" }))
+            handleIngredientUpdate(false, cardState.id)
         }
     }
 
     function handleInputChange(e) {
-        const inputValue = e.target.value.toUpperCase();
-        setInputValues(prev => ({ ...prev, current: inputValue }));
-        setSuggestions(ingredients.filter(ingredient => ingredient.name.toUpperCase().includes(inputValue)));
+        const inputValue = e.target.value.toUpperCase()
+        setInputValues(prev => ({ ...prev, current: inputValue }))
+        setSuggestions(ingredients.filter(ingredient => ingredient.name.toUpperCase().includes(inputValue)))
     }
 
     function handleSuggestionClick(e, id) {
         e.stopPropagation();
-        const clickedElement = ingredients.find(ingredient => ingredient.id === id);
+        const clickedElement = ingredients.find(ingredient => ingredient.id === id)
         if (!clickedElement.isSelected && !displayedIngredients.some(ingredient => ingredient.id === id)) {
-            const { name, id, bgColor } = clickedElement;
-            setCardState({ label: name, id, state: true, color: bgColor, inputActive: false });
-            setInputValues(prev => ({ ...prev, current: name }));
-            handleIngredientUpdate(false, cardState.id);
-            handleIngredientUpdate(true, id);
+            const { name, id, bgColor } = clickedElement
+            setCardState({ label: name, id, state: true, color: bgColor, inputActive: false })
+            setInputValues(prev => ({ ...prev, current: name }))
+            handleIngredientUpdate(false, cardState.id)
+            handleIngredientUpdate(true, id)
         }
     }
 
@@ -61,45 +61,45 @@ export function useIngredientCard(label, id, isSelected, bgColor) {
         const inputValue = e.target.value.toUpperCase();
         const isInDatabase = ingredients.filter(ingredient =>
             ingredient.name.toUpperCase().includes(inputValue) && !ingredient.isSelected
-        );
+        )
 
         const isInDisplay = displayedIngredients.filter(displayedIngredient =>
             isInDatabase.some(dbIngredient =>
                 dbIngredient.id === displayedIngredient.id ||
                 dbIngredient.name.toUpperCase() === displayedIngredient.name.toUpperCase()
             )
-        );
+        )
 
         const firstAvailableIngredient = isInDatabase.find(ingredient =>
             !isInDisplay.some(displayedIngredient => displayedIngredient.id === ingredient.id)
-        );
+        )
 
         if (inputValue !== "" && firstAvailableIngredient) {
             const { name, id, bgColor } = firstAvailableIngredient;
             setCardState({ label: name, id, state: true, color: bgColor, inputActive: false });
-            setInputValues(prev => ({ ...prev, current: name }));
+            setInputValues(prev => ({ ...prev, current: name }))
             handleIngredientUpdate(true, id);
         } else {
-            setInputValues(prev => ({ ...prev, current: prev.initial }));
-            setCardState(prev => ({ ...prev, inputActive: false }));
-            handleIngredientUpdate(cardState.state, cardState.id);
+            setInputValues(prev => ({ ...prev, current: prev.initial }))
+            setCardState(prev => ({ ...prev, inputActive: false }))
+            handleIngredientUpdate(cardState.state, cardState.id)
         }
-        setSuggestions(ingredients);
+        setSuggestions(ingredients)
     }
 
     function handlePressEnter(e) {
         if (e.keyCode === 13) {
-            e.target.blur();
+            e.target.blur()
         }
     }
 
     function handleXClick(e) {
-        e.stopPropagation();
+        e.stopPropagation()
         if (cardState.inputActive) {
-            setInputValues(prev => ({ ...prev, current: "" }));
-            setCardState(prev => ({ ...prev, inputActive: false }));
+            setInputValues(prev => ({ ...prev, current: "" }))
+            setCardState(prev => ({ ...prev, inputActive: false }))
         } else {
-            handleIngredientsDecrement(cardState.id, e);
+            handleIngredientsDecrement(cardState.id, e)
         }
     }
 
