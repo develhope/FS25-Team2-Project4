497905c4e2a067705b091fe9b3823bc366f658d4
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-05-21 17:10:21 +0200

    User experience improvement
    -Added an icon to show the user when an inactive search suggestion is a blocked ingredient or a selected one

--- recipe-roulette.js/src/components/Search/Suggestions/IngredientSuggestionInactive.jsx
+++ recipe-roulette.js/src/components/Search/Suggestions/IngredientSuggestionInactive.jsx
@@ -1,7 +1,14 @@
+import { MaterialSymbol } from "react-material-symbols"
 import classes from "./IngredientSuggestions.module.scss"
 
 export function IngredientSuggestionInactive({ ing }) {
     const { id, name, bgColor, isSelected, isBlackListed } = ing
 
-    return <p className={`${classes.inactiveSuggestion} ${classes.ingredientSuggestion}`}>{name}</p>
+    return (
+        <p className={`${classes.inactiveSuggestion} ${classes.ingredientSuggestion}`}>
+            {name}
+            {ing.isBlackListed && <MaterialSymbol icon="block" grade={18} size={18} />}
+            {ing.isSelected && <MaterialSymbol icon="check_circle" grade={18} size={18} />}
+        </p>
+    )
 }
