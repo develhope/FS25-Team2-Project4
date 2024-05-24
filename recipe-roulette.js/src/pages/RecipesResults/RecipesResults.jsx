22dffa34a08481f35b6c510a9feea356a2d77f0d
alessandro-canonico <alessandrocanonico96@gmail.com>
2024-05-24 21:03:24 +0200

    start creating results page

--- recipe-roulette.js/src/pages/RecipesResults/RecipesResults.jsx
+++ recipe-roulette.js/src/pages/RecipesResults/RecipesResults.jsx
@@ -1,10 +1,30 @@
 import classes from "./RecipesResults.module.scss"
 import RecipeCard from "../../components/RecipeCard/RecipeCard"
+import { FilterChip } from "../../components/FilterChip/FilterChip"
+import { useFilterChips } from "../../components/FilterChip/useFilterChip"
 
 function RecipeResults() {
+    const { handleDeselectChip } = useFilterChips()
+
+
     return (
         <div className={classes.recipesResultsPage}>
-            <h1>Recipe Results</h1>
+            <header>
+                <h1>Results</h1>
+                <button className={classes.btnFilters}>filters</button>
+            </header>
+            <div className={classes.subHeading}>
+                <p className={classes.text}>Preparation time</p>
+                <div className={classes.chipBox}>
+                <FilterChip/>
+                <FilterChip/>
+                <FilterChip/>
+            </div>
+            </div>
+
+            <div className={classes.resultsBox}>
+
+            </div>
             <RecipeCard title="Just a title" />
             <RecipeCard title="Just another title" />
             <RecipeCard title="Just anotherother title" />
