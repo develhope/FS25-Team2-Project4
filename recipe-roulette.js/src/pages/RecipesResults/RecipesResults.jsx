cfbeeb65b0697f4faa9da22c15b64ccb4b937b49
alessandro-canonico <alessandrocanonico96@gmail.com>
2024-05-25 20:47:38 +0200

    implement logic btn filters, optimized layout and style

--- recipe-roulette.js/src/pages/RecipesResults/RecipesResults.jsx
+++ recipe-roulette.js/src/pages/RecipesResults/RecipesResults.jsx
@@ -1,35 +1,84 @@
-import classes from "./RecipesResults.module.scss"
-import RecipeCard from "../../components/RecipeCard/RecipeCard"
-import { FilterChip } from "../../components/FilterChip/FilterChip"
-import { useFilterChips } from "../../components/FilterChip/useFilterChip"
+import classes from "./RecipesResults.module.scss";
+import RecipeCard from "../../components/RecipeCard/RecipeCard";
+import { useState } from "react";
+import { MaterialSymbol } from "react-material-symbols";
 
 function RecipeResults() {
-    const { handleDeselectChip } = useFilterChips()
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(null);
 
+  function handleClick(index) {
+    setSelectedButtonIndex((prevIndex) => (prevIndex === index ? null : index));
+  }
 
-    return (
-        <div className={classes.recipesResultsPage}>
-            <header>
-                <h1>Results</h1>
-                <button className={classes.btnFilters}>filters</button>
-            </header>
-            <div className={classes.subHeading}>
-                <p className={classes.text}>Preparation time</p>
-                <div className={classes.chipBox}>
-                <FilterChip/>
-                <FilterChip/>
-                <FilterChip/>
-            </div>
-            </div>
-
-            <div className={classes.resultsBox}>
-
-            </div>
-            <RecipeCard title="Just a title" />
-            <RecipeCard title="Just another title" />
-            <RecipeCard title="Just anotherother title" />
+  return (
+    <div className={classes.recipesResultsPage}>
+      <header>
+        <h1>Results</h1>
+        <button className={classes.btnFilters}>
+          <MaterialSymbol
+            className={classes.icon}
+            icon="tune"
+            size={20}
+            grade={18}
+          />
+          Filters
+        </button>
+      </header>
+      <div className={classes.subHeading}>
+        <p className={classes.text}>Preparation time</p>
+        <div className={classes.chipBox}>
+          <button
+            onClick={() => handleClick(0)}
+            className={selectedButtonIndex === 0 ? classes.selected : ""}
+          >
+            {selectedButtonIndex === 0 && (
+              <MaterialSymbol
+                className={classes.icon}
+                icon="check"
+                size={22}
+                grade={18}
+              />
+            )}
+            <p></p>
+            30 min
+          </button>
+          <button
+            onClick={() => handleClick(1)}
+            className={selectedButtonIndex === 1 ? classes.selected : ""}
+          >
+            {selectedButtonIndex === 1 && (
+              <MaterialSymbol
+                className={classes.icon}
+                icon="check"
+                size={22}
+                grade={18}
+              />
+            )}
+            <p>45 min</p>
+          </button>
+          <button
+            onClick={() => handleClick(2)}
+            className={selectedButtonIndex === 2 ? classes.selected : ""}
+          >
+            {selectedButtonIndex === 2 && (
+              <MaterialSymbol
+                className={classes.icon}
+                icon="check"
+                size={22}
+                grade={18}
+              />
+            )}
+            <p>60 min +</p>
+          </button>
         </div>
-    )
+      </div>
+
+      <div className={classes.resultsBox}></div>
+      <RecipeCard title="Just a title" />
+      <RecipeCard title="Just another title" />
+      <RecipeCard title="Just anotherother title" />
+    </div>
+  );
 }
 
-export default RecipeResults
+export default RecipeResults;
