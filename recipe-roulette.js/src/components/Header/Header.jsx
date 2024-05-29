d36b25f6deb0bc07675f3f5df224e26ca4a39bb1
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-05-29 19:57:51 +0200

    update header component - add filter button to recipes-results page

--- recipe-roulette.js/src/components/Header/Header.jsx
+++ recipe-roulette.js/src/components/Header/Header.jsx
@@ -1,53 +1,59 @@
-import { useLocation } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-import classes from "./Header.module.scss"
-import { MaterialSymbol } from "react-material-symbols"
+import classes from "./Header.module.scss";
+import { MaterialSymbol } from "react-material-symbols";
 
 export function Header({ handleMenuToggle }) {
-    const [title, setTitle] = useState("/")
-    const location = useLocation()
+  const [title, setTitle] = useState("/");
+  const location = useLocation();
 
-    useEffect(() => {
-        switch (location.pathname) {
-            case "/":
-                setTitle("Welcome!")
-                break
-            case "/discovery":
-                setTitle("Discovery")
-                break
-            case "/favorited":
-                setTitle("Favorited")
-                break
-            case "/settings":
-                setTitle("Settings")
-                break
-            case "/recipes-results":
-                    setTitle("Results")
-                    break
-        }
-    }, [location.pathname])
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        setTitle("Welcome!");
+        break;
+      case "/discovery":
+        setTitle("Discovery");
+        break;
+      case "/favorited":
+        setTitle("Favorited");
+        break;
+      case "/settings":
+        setTitle("Settings");
+        break;
+      case "/recipes-results":
+        setTitle("Results");
+        break;
+    }
+  }, [location.pathname]);
 
-    return (
-        <header>
-            <div className={classes.leftItems}>
-                <MaterialSymbol
-                    onClick={handleMenuToggle}
-                    className={classes.menu}
-                    icon="menu"
-                    weight={500}
-                    size={24}
-                    grade={24}
-                />
-                <h1>{title}</h1>
-            </div>
+  return (
+    <header>
+      <div className={classes.leftItems}>
+        <MaterialSymbol
+          onClick={handleMenuToggle}
+          className={classes.menu}
+          icon="menu"
+          weight={500}
+          size={24}
+          grade={24}
+        />
+        <h1>{title}</h1>
+      </div>
 
-            {location.pathname === "/favorited" && (
-                <button className={classes.leftIcoButton}>
-                    <MaterialSymbol icon="tune" />
-                    Filters
-                </button>
-            )}
-        </header>
-    )
+      {location.pathname === "/favorited" && (
+        <button className={classes.leftIcoButton}>
+          <MaterialSymbol icon="tune" />
+          Filters
+        </button>
+      )}
+      {location.pathname === "/recipes-results" && (
+        <button className={classes.leftIcoButton}>
+          <MaterialSymbol icon="tune" />
+          Filters
+        </button>
+      )}
+    </header>
+  );
 }
