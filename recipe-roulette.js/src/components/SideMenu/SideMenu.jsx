b23231736eafb2b536c6b95382f567900ea076a1
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-06-06 16:27:45 +0200

    implement snackbar for login

--- recipe-roulette.js/src/components/SideMenu/SideMenu.jsx
+++ recipe-roulette.js/src/components/SideMenu/SideMenu.jsx
@@ -1,88 +1,89 @@
-import { NavigationLink } from "./NavigationLink/NavigationLink"
+import { NavigationLink } from "./NavigationLink/NavigationLink";
 
-import BookmarksOutlinedIcon from "@mui/icons-material/BookmarksOutlined"
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined"
-import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined"
-import MenuOpenOutlinedIcon from "@mui/icons-material/MenuOpenOutlined"
-import AutorenewIcon from "@mui/icons-material/Autorenew"
-import LogoutIcon from "@mui/icons-material/Logout"
-import { useAuth } from "../../hooks/Auth/useAuth"
+import BookmarksOutlinedIcon from "@mui/icons-material/BookmarksOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
+import MenuOpenOutlinedIcon from "@mui/icons-material/MenuOpenOutlined";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useAuth } from "../../hooks/Auth/useAuth";
 
-import classes from "./SideMenu.module.scss"
-import { Snackbar } from "../Snackbar/Snackbar"
-import { useState } from "react"
+import classes from "./SideMenu.module.scss";
+import { Snackbar } from "../Snackbar/Snackbar";
+import { useSnackbar } from "../Snackbar/useSnackbar";
 
 export function SideMenu({ handleMenuToggle, menuState = false, path = "/" }) {
-    const { logout, isAuthenticated } = useAuth()
-    const [showSnackbar, setShowSnackbar] = useState(false);
+  const { logout, isAuthenticated } = useAuth();
+  const { handleOpenSnackbar } = useSnackbar();
 
-    function handleSnackBar () {
-        if (isAuthenticated) {
-            setShowSnackbar(false)
-        } else {
-            setShowSnackbar(true)
-            console.log(`ciao`);
+    function handleClickSnackBar () {
+        if (!isAuthenticated) {
+            handleOpenSnackbar("Log")
         }
     }
 
-    return (
-        <div>
-            <div
-                onClick={handleMenuToggle}
-                className={`${classes.backgroundOverlay} ${menuState && classes.backgroundOverlayToggled}`}
-            ></div>
-            <div className={`${classes.sidebar} ${menuState && classes.sidebarToggled}`}>
-                <header>
-                    <h4>Browse</h4>
-                    <div onClick={handleMenuToggle} className={classes.closeIco}>
-                        <MenuOpenOutlinedIcon />
-                    </div>
-                </header>
-                <section className={classes.links}>
-                    <NavigationLink
-                        path={path}
-                        handleMenuToggle={handleMenuToggle}
-                        label="Roulette"
-                        destination="/discovery"
-                        icon={<AutorenewIcon fontSize="small" />}
-                    />
-                    <NavigationLink
-                        path={path}
-                        handleMenuToggle={handleMenuToggle}
-                        label="Favorited"
-                        destination={isAuthenticated && "/favorited"}
-                        icon={<BookmarksOutlinedIcon fontSize="small" />}
-                        action={handleSnackBar}
-                    />
-                    <NavigationLink
-                        path={path}
-                        handleMenuToggle={handleMenuToggle}
-                        label="Settings"
-                        destination="/settings"
-                        icon={<SettingsOutlinedIcon fontSize="small" />}
-                    />
-                    <div className={classes.separator} />
-                    {isAuthenticated ? (
-                        <NavigationLink
-                            path={path}
-                            handleMenuToggle={handleMenuToggle}
-                            label={"Logout"}
-                            destination={"/login"}
-                            icon={<LogoutIcon fontSize="small" />}
-                            action={logout}
-                        />
-                    ) : (
-                        <NavigationLink
-                            path={path}
-                            handleMenuToggle={handleMenuToggle}
-                            label="Login"
-                            destination="/login"
-                            icon={<LoginOutlinedIcon fontSize="small" />}
-                        />
-                    )}
-                </section>
-            </div>
-            {showSnackbar && <Snackbar/>}
-        </div>
-    )
+  return (
+    <div>
+      <div
+        onClick={handleMenuToggle}
+        className={`${classes.backgroundOverlay} ${
+          menuState && classes.backgroundOverlayToggled
+        }`}
+      ></div>
+      <div
+        className={`${classes.sidebar} ${menuState && classes.sidebarToggled}`}
+      >
+        <header>
+          <h4>Browse</h4>
+          <div onClick={handleMenuToggle} className={classes.closeIco}>
+            <MenuOpenOutlinedIcon />
+          </div>
+        </header>
+        <section className={classes.links}>
+          <NavigationLink
+            path={path}
+            handleMenuToggle={handleMenuToggle}
+            label="Roulette"
+            destination="/discovery"
+            icon={<AutorenewIcon fontSize="small" />}
+          />
+          <NavigationLink
+            path={path}
+            handleMenuToggle={handleMenuToggle}
+            label="Favorited"
+            destination={isAuthenticated && "/favorited"}
+            icon={<BookmarksOutlinedIcon fontSize="small" />}
+            action={handleClickSnackBar}
+          />
+          <NavigationLink
+            path={path}
+            handleMenuToggle={handleMenuToggle}
+            label="Settings"
+            destination="/settings"
+            icon={<SettingsOutlinedIcon fontSize="small" />}
+          />
+          <div className={classes.separator} />
+          {isAuthenticated ? (
+            <NavigationLink
+              path={path}
+              handleMenuToggle={handleMenuToggle}
+              label={"Logout"}
+              destination={"/login"}
+              icon={<LogoutIcon fontSize="small" />}
+              action={logout}
+            />
+          ) : (
+            <NavigationLink
+              path={path}
+              handleMenuToggle={handleMenuToggle}
+              label="Login"
+              destination="/login"
+              icon={<LoginOutlinedIcon fontSize="small" />}
+            />
+          )}
+        </section>
+      </div>
+      <Snackbar />
+    </div>
+  );
 }
