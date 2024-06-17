b36fbe5453b6d97173077762eca38d9b53e7c75b
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-06-17 16:34:21 +0200

    login and signup CSS update

--- recipe-roulette.js/src/pages/Login/LoginPage.jsx
+++ recipe-roulette.js/src/pages/Login/LoginPage.jsx
@@ -5,11 +5,14 @@ import { useLocationHook } from "../../hooks/useLocationHook"
 import classes from "./LoginPage.module.scss"
 
 export function LoginPage() {
-    const {location } = useLocationHook()
+    const { location } = useLocationHook()
     const { animate } = useAnimate(location)
 
     return (
         <section className={`${classes.pageBox} ${animate && classes.animateLoginPage}`}>
+            <h2>
+                Welcome back to <br/> <span>RecipeRoulette!</span>
+            </h2>
             <Login prevLocation={location.pathname} />
         </section>
     )
