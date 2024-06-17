44d72b343bf94f413e925fc06f7c956e727bc9e8
alessandro-canonico <alessandrocanonico96@gmail.com>
2024-06-17 11:46:47 +0200

    added pop up in settings page

--- recipe-roulette.js/src/pages/Settings/Linkbox.jsx
+++ recipe-roulette.js/src/pages/Settings/Linkbox.jsx
@@ -1,33 +1,68 @@
-import { Button } from "../../components/Buttons/Button/Button"
+import { Button } from "../../components/Buttons/Button/Button";
 
-import LogoutIcon from "@mui/icons-material/Logout"
-import NavigateNextOutlinedIcon from "@mui/icons-material/NavigateNextOutlined"
+import LogoutIcon from "@mui/icons-material/Logout";
+import LoginIcon from "@mui/icons-material/Login";
+import NavigateNextOutlinedIcon from "@mui/icons-material/NavigateNextOutlined";
 
-import classes from "./Settings.module.scss"
-import { Link } from "react-router-dom"
+import classes from "./Settings.module.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import { useAuth } from "../../hooks/Auth/useAuth";
+import { PopupLogin } from "../../components/Pop-up/Popup"
+import { Login } from "../../components/authentication/login/Login";
 
 export function LinkBox({ handleLogoutClick }) {
-    return (
-        <>
-            <div className={classes.menuSection}>
-                <Link to="/food-preferences" className={classes.menuItem}>
-                    Food Preferences
-                    <NavigateNextOutlinedIcon fontSize="medium" />
-                </Link>
+  const [showPopup, setShowPopup] = useState();
+  const { isAuthenticated } = useAuth();
 
-                <Link to="/recipes-hisory" className={classes.menuItem}>
-                    Recipes History
-                    <NavigateNextOutlinedIcon fontSize="medium" />
-                </Link>
+  return (
+    <>
+      <div className={classes.menuSection}>
+        <Link to="/food-preferences" className={classes.menuItem}>
+          Food Preferences
+          <NavigateNextOutlinedIcon fontSize="medium" />
+        </Link>
 
-                <Link to="/feedback-&-support" className={classes.menuItem}>
-                    Feedback & Support
-                    <NavigateNextOutlinedIcon fontSize="medium" />
-                </Link>
-            </div>
-            <div className={classes.logoutButtonWrapper}>
-                <Button label="Logout" width="fill" action={handleLogoutClick} icon={<LogoutIcon fontSize="small" />} link="login" />
-            </div>
-        </>
-    )
+        <Link to="/recipes-hisory" className={classes.menuItem}>
+          Recipes History
+          <NavigateNextOutlinedIcon fontSize="medium" />
+        </Link>
+
+        <Link to="/feedback-&-support" className={classes.menuItem}>
+          Feedback & Support
+          <NavigateNextOutlinedIcon fontSize="medium" />
+        </Link>
+      </div>
+      <div className={classes.logoutButtonWrapper}>
+        {isAuthenticated ? (
+          <Button
+            label="Logout"
+            width="fill"
+            action={handleLogoutClick}
+            icon={<LogoutIcon fontSize="small" />}
+            link="login"
+          />
+        ) : (
+          <Button
+            type="submit"
+            style="primary"
+            label="Login"
+            width="fill"
+            icon={<LoginIcon fontSize="small" />}
+            action={(e) => {
+              setShowPopup(true);
+            }}
+          />
+        )}
+      </div>
+      {showPopup &&
+        createPortal(
+          <PopupLogin handleClosePopup={() => setShowPopup(false)}>
+            <Login setShowPopup={setShowPopup} />
+          </PopupLogin>,
+          document.getElementById("popup-root")
+        )}
+    </>
+  );
 }
