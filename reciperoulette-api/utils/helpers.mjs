774f80cf58c7561cc809f7d1f2483d0ffed5c058
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-06-25 12:10:22 +0200

    move db connection in utils/helpers.mjs

--- reciperoulette-api/utils/helpers.mjs
+++ reciperoulette-api/utils/helpers.mjs
@@ -1,5 +1,12 @@
 // può contenere i metodi da riutilizzare
+import pgPromise from "pg-promise"
+import dotenv from "dotenv"
 
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient()
+
+dotenv.config()
+const url = process.env.DATABASE_URL
+
+export const db = pgPromise()(url)
