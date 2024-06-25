75479eb0a8a3fae1405f11e76eb87e59f48a6baf
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-06-25 13:01:12 +0200

    Backend update
    -Added singup function to sign up new user.
    -if the username or email already exists, the user will receive an error
    -Otherwise their data will be added to the users table

--- reciperoulette-api/controllers/users-controllers.mjs
+++ reciperoulette-api/controllers/users-controllers.mjs
@@ -15,6 +15,25 @@ const getUsers = async (req, res) => {
     }
 }
 
-const signup = (req, res) => {}
+const signup = async (req, res) => {
+    try {
+        const { email, username, password } = req.body
+        const emailExists = await db.oneOrNone(`SELECT email FROM users WHERE email=$1`, email)
+        const usernameExists = await db.oneOrNone(`SELECT username FROM users WHERE username=$1`, username)
+        if (!emailExists && !usernameExists) {
+            const { id } = await db.one(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id;`, [
+                username,
+                email,
+                password,
+            ])
+            res.status(201).json({ id, msg: "User created successfully" })
+        } else {
+            res.status(400).json({ msg: "An user with this email or username already exists" })
+        }
+    } catch (error) {
+        console.log()
+        res.status(500).json({ msg: "Internal server error" })
+    }
+}
 
 export { getUsers, signup }
