e3c4a2300060b3fcff456ffab6483d9a4a8f6863
alessandro-canonico <alessandrocanonico96@gmail.com>
2024-06-28 21:27:11 +0200

    Implementation fetch function
    - set fetch function in useLogin hook
    - implement Reac Query library
    - using useMutation to handle fetch

--- recipe-roulette.js/src/Root.jsx
+++ recipe-roulette.js/src/Root.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'react-material-symbols/outlined'
 
 import App from "./App"
 
+const queryClient = new QueryClient()
+
 export function Root() {
     return (
+        <QueryClientProvider client={queryClient}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
+        </QueryClientProvider>
     )
 }
